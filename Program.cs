2fcb88e636b3ab3feac9cc6625a1df169e8b8588
Geoffrey Miller <gmiller@vailsys.com>
2019-10-30 13:46:29 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using com.freeclimb.api;
+
+namespace DeleteRecording {
+  class Program {
+    static void Main (string[] args) {
+      try {
+        string freeClimbAccountId = System.Environment.GetEnvironmentVariable("ACCOUNT_ID");
+        string freeClimbAccountToken = System.Environment.GetEnvironmentVariable("AUTH_TOKEN");
+        string recordingId = "";
+
+        // Create FreeClimbClient object
+        FreeClimbClient client = new FreeClimbClient (freeClimbAccountId, freeClimbAccountToken);
+
+        // Invoke deleted method to delete recording Url
+        client.getRecordingsRequester.delete (recordingId);
+      } catch (FreeClimbException ex) {
+        // Exception throw upon failure
+        System.Console.WriteLine(ex.Message);
+      }
+    }
+  }
+}
